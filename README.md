1277e03ca88358308cba248fe93a7d9250a23d27
Thet Hlaing <th.ucsy@gmail.com>
2017-05-16 17:03:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# coffeehit-map
---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('caffeinehit', [
+	'ionic',
+	'ngMap',
+	'ngCordova',
+	'caffeinehit.controllers',
+	'caffeinehit.services',
+	'caffeinehit.filters'
+]);
+
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+app.config(function ($httpProvider) {
+	$httpProvider.defaults.headers.common['Authorization'] = 'Token fda42eee67a62db5fe4499fc1bbae128b04c2a6a';
+});
